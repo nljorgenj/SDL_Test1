4767fae12d4e5bc022c9692a19e1c3bc75be9917
nljor <nljor@JONSSONS>
2017-03-11 11:07:59 +0100

    Add project files.

---
+++ SDL_Test1/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <SDL.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL init failed." << endl;
+		return 1;
+	}
+
+	cout << "SDL Init succeeded." << endl;
+
+	SDL_Quit();
+
+	// system("PAUSE");
+
+	return 0;
+}
+
